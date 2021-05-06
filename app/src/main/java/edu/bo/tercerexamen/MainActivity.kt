c86f14c5ac726062e2fc1c052295825417dde78d
Jose Carlos <josecarlos.jcop.3@gmail.com>
2021-05-06 00:15:48 -0400

    Finish Exam

--- app/src/main/java/edu/bo/tercerexamen/MainActivity.kt
+++ app/src/main/java/edu/bo/tercerexamen/MainActivity.kt
@@ -21,9 +21,7 @@ class MainActivity : AppCompatActivity() {
 //            repository.insert(Book("El Señor de los Anillos","BF67","J.R.R. Tolkien","23/09/1999",190,"Un buen libro","https://i2.wp.com/elanillounico.com/wp-content/uploads/2020/01/Pases-trilogia-extendida-El-Se%C3%B1or-de-los-Anillos-Barcelona-copia.jpg?w=660&ssl=1"))
 //            repository.insert(Book("El Código da Vinci ","CJ65","Paulo Coelho","07/10/2010",230,"Buenisimo","https://m.media-amazon.com/images/I/51vC0FsLN6L._SY346_.jpg"))
             val list = repository.getListBooks()
-            list.forEach {
-                Log.d("DBTEST","Id book = ${it.id}, Title: ${it.title}, Pages: ${it.pages},Editorial: ${it.isbn}, Author: ${it.author}")
-            }
+
             val adLayoutManager= LinearLayoutManager(applicationContext)
             adLayoutManager.orientation= LinearLayoutManager.VERTICAL
 
